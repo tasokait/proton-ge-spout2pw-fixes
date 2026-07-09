65f21a48d786cdbe734d4ce7cba1899ea3319b4e
Kaitsuki Tasogare <167069558+tasokait@users.noreply.github.com>
2026-07-09 23:33:29 +0400

    spout2pw doesn't need wine patch for working on proton/wine 11 anymore, technically there is no reason for existence of this proton build anymore, but better to keep it around for now for stability reasons Co-authored-by: Hoshino Lina <lina@lina.yt>

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -294,9 +294,6 @@ apply_all_in_dir() {
     apply_patch "../patches/wine-hotfixes/pending/0001-server-Dynamically-relocate-.exes-by-default-too.patch"
     apply_patch "../patches/wine-hotfixes/pending/0002-ntdll-allow-disabling-executable-ASLR.patch"
 
-    echo "WINE: -CUSTOM- added d3dkmt dma buf fd request to wineserver"
-    apply_patch "../patches/wine-hotfixes/pending/d3dkmt_fd_wineserver_export.patch"
-
 ### END WINE PENDING UPSTREAM SECTION ###
 
 
